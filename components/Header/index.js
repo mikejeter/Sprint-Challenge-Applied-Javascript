7839ffb698072d46449266b60336c1b339553d09
mikejeter <mjeter45@yahoo.com>
2019-10-29 13:59:13 -0400

    Modified Header/index.js, Tabs/index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // STEP 1: Create a header component.
 // -----------------------
 // Using a function create the component you see below:
@@ -38,53 +37,3 @@ const hDiv = newHeader();
 const headerContainer = document.querySelector('.header-container');
 // const header = document.querySelector('.header');
 headerContainer.appendChild(hDiv);
-
-
-
-
-
-
-
-
-
-=======
-// STEP 1: Create a header component.
-// -----------------------
-// Using a function create the component you see below:
-//
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div >
-// And add it to the DOM in the .header-container component
-
-function Header() {
-    const hDiv = document.createElement('div');
-    const hSpan = document.createElement('span');
-    const hH1 = document.createElement('h1');
-    const hSpan2 = document.createElement('span');
-
-    hDiv.appendChild(hSpan);
-    hDiv.appendChild(hH1);
-    hDiv.appendChild(hSpan2);
-
-    hSpan.textContent = date;
-    hH1.textContent = h1;
-    hSpan2.textContent = temp;
-
-    hDiv.classList.add('header');
-    hSpan.classList.add('date');
-    hH1.classList.add('h1');
-    hSpan2.classList.add('temp');
-
-
-    return Header;
-}
-
-const header = document.querySelector('.header-container');
-
-
-
-
->>>>>>> 2c9034e01915bca513db29a03f3c75d34eacedcd
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Step 2: Create Tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
@@ -33,40 +32,3 @@ axios.get('https://lambda-times-backend.herokuapp.com/topics')
 
         return tabClass;
     }
-    // const tabClass = createTabs();
-
-    
-    // newTopics.appendChild(tabClass);
-=======
-// Step 2: Create Tabs
-// -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is returned console.log it and review the structure.
-// Iterate over the topics creating a new Tab component and add it to the DOM
-// under the .topics element.
-//
-//  The tab component should look like this:
-//    <div class="tab">topic here</div>
-
-
-const topics = document.querySelector('topics');
-
-axios
-.get('https://lambda-times-backend.herokuapp.com/topics')
-.then(results => {
-    console.log(results);
-    results.topics.forEach(topic => {
-        topics.appendChild(createTabs(topic));
-    });
-}).catch((err) => {
-    console.log(err);
-});
-
-    function createTabs(topic) {
-        const tabClass = document.createElement('div');
-        tabClass.classList.add('tab');
-        tabClass.textContent = topic;
-
-        return createTabs;
-    }
->>>>>>> 2c9034e01915bca513db29a03f3c75d34eacedcd
